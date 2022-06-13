7d6b5d0beeec03ab6b168b0ff7e4c487b20a1c3d
Tranthuhang1 <106530910+Tranthuhang1@users.noreply.github.com>
2022-06-13 18:29:59 +0700

    Add files via upload

---
+++ 130622.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+
+int a=10;
+float b=24.67892345;
+char ch= 'A';
+printf("\ninterger data = %d",a);
+printf("\nFloat data = %f",b);
+printf("\ncharater data = %c",ch);
+printf("\nthis prints the string");
+printf("%s","\nthis prints the string");
+	
+}
